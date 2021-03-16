b376558d4cb941cb8a9be553ea7e80b7a329fb91
dboren <danielboren@att.net>
2021-03-16 17:06:43 -0500

    added get route for notes.html

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/develop/public/index.html'));
 })
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
+})
 
 
 app.listen(PORT, () => {
