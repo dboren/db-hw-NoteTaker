9715868f8aca0cb7017a4085a8dd86a1f94006d2
dboren <danielboren@att.net>
2021-03-16 18:01:32 -0500

    added fs to server file and added api path to get saved notes. Still debugging html get path for notes page

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const fs = require('fs');
+
+const savedNotes = require('/develop/db/db');
 
 
 app.get('*', (req, res) => {
@@ -12,6 +15,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
 })
 
+app.get('/api/notes', (req, res) => res.json(savedNotes));
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
