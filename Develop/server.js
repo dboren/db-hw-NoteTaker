81718f57747ebd0a3c0b90e3de829ccbe364e9d7
dboren <danielboren@att.net>
2021-03-17 15:33:31 -0500

    moved server.js to develop folder and adjusted get paths accordingly, but the notes page is still not loading

--- Develop/server.js
+++ Develop/server.js
@@ -4,16 +4,16 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const fs = require('fs');
 
-let savedNotes = require('/develop/db/db');
+//let savedNotes = require('/develop/db/db');
 
 //routing
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/develop/public/index.html'));
-})
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
-})
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('/api/notes', (req, res) => res.json(savedNotes));
 
