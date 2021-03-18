177a1dfc649d3057d30ad16e6322f5f4a5c84eb2
dboren <danielboren@att.net>
2021-03-17 21:54:46 -0500

    changed get index path to / instead of * to correct notes routing issue. Added urlencoded and other app.use lines to allow functionality

--- Develop/server.js
+++ Develop/server.js
@@ -4,10 +4,14 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const fs = require('fs');
 
-//let savedNotes = require('/develop/db/db');
+const db = require('./db/db');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 //routing
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
@@ -15,11 +19,15 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('/api/notes', (req, res) => res.json(savedNotes));
+app.get('/api/notes', (req, res) => res.json(db));
 
 app.post('/api/notes', (req, res) => {
-    savedNotes.push(req.body);
-})
+    db.push(json.stringify(req.body));
+});
+
+// app.put('/api/notes', (req, res) => {
+//     savedNotes./*determine method*/(req.body);
+// })
 // End of routing
 
 app.listen(PORT, () => {
