6c517ac5044abd0320b1886bd1aec70bde37ba34
dboren <danielboren@att.net>
2021-03-19 17:27:32 -0500

    installed generate-unique-id npm package

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const fs = require('fs');
-
+const generateUeniqueID = require('generate-unique-id');
 const db = require('./db/db');
 
 app.use(express.urlencoded({ extended: true }));
@@ -25,9 +25,9 @@ app.post('/api/notes', (req, res) => {
     db.push(json.stringify(req.body));
 });
 
-// app.put('/api/notes', (req, res) => {
-//     savedNotes./*determine method*/(req.body);
-// })
+app.put('/api/notes', (req, res) => {
+    db.push(req.body);
+})
 // End of routing
 
 app.listen(PORT, () => {
